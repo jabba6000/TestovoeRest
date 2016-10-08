020ac41141dabccbe2647840d3ff674699de4d8b
Uri Fuholichev <Uri@Uris-Mac-mini.local>
2016-10-08 13:54:24 +0300

    0.01 - pod installed

---
+++ TestovoeRestaurant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestovoeRestaurant
+//
+//  Created by Uri Fuholichev on 10/8/16.
+//  Copyright © 2016 Andrei Karpenia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
