80effff2fecb8ab84bb29ace7f73f00fa808376e
Uri Fuholichev <Uri@Uris-Mac-mini.local>
2016-10-09 18:43:13 +0300

    now running good

--- TestovoeRestaurant/DishCardViewController.m
+++ TestovoeRestaurant/DishCardViewController.m
@@ -17,22 +17,16 @@
 @property (strong, nonatomic) IBOutlet UILabel *dishPrice;
 @property (strong, nonatomic) IBOutlet UILabel *dishDescription;
 
-@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
-
 @end
 
 @implementation DishCardViewController
 
 -(void) viewDidLoad{
-    
-    NSLog(@"%@", [_dish objectForKey:@"name"]);
-    NSLog(@"%@", [_dish objectForKey:@"weight"]);
-    NSLog(@"%@", [_dish objectForKey:@"price"]);
-    NSLog(@"%@", [_dish objectForKey:@"description"]);
 
     _dishName.text = [_dish objectForKey:@"name"];
     _dishWeight.text = [NSString stringWithFormat:@"Вес: %@", [_dish objectForKey:@"weight"]];
     _dishPrice.text = [NSString stringWithFormat:@"Цена: %@",[_dish objectForKey:@"price"]];
+   
     if ([[_dish objectForKey:@"description"] isEqual:@""])
         _dishDescription.text = @"Описание отсутствует";
     else
@@ -46,26 +40,18 @@
         }
     else{
         _dishImageView.image = [UIImage imageNamed:@"logo.png"];
-        NSLog(@"No image");
-        dispatch_async(dispatch_get_main_queue(), ^{
-            
+        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0ul);
+        dispatch_async(queue, ^(void) {
             NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:[_dish objectForKey:@"picture"]]];
-            
             UIImage* image = [[UIImage alloc] initWithData:imageData];
-            
-            [[DataCollector sharedInstance].dishesImagesDictionary setObject:image forKey:[_dish objectForKey:@"name"]];
-            _dishImageView.image = [[DataCollector sharedInstance].dishesImagesDictionary objectForKey:[_dish objectForKey:@"name"]];
+            if (image){
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [[DataCollector sharedInstance].dishesImagesDictionary setObject:image forKey:[_dish objectForKey:@"name"]];
+                    _dishImageView.image = [[DataCollector sharedInstance].dishesImagesDictionary objectForKey:[_dish objectForKey:@"name"]];
+                });
+            }
         });
     }
-
-}
-
-- (void) viewDidAppear{
-    
-    _dishName.text = [_dish objectForKey:@"name"];
-    _dishWeight.text = [_dish objectForKey:@"weight"];
-    _dishPrice.text = [_dish objectForKey:@"price"];
-    _dishDescription.text = [_dish objectForKey:@"description"];
 }
 
 @end
